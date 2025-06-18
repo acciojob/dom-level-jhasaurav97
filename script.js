a8cae24a577c8c177da092ade1cfc2ad3d15501c
Saurav <jhasaurav593@gmail.com>
2025-06-18 19:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const list = document.getElementById("level");
+const allList = list.parentElement.children;
+let index;
+for(let i = 0; i < allList.length; i++){
+	if(allList[i] === list){
+		index = i;
+		break;
+	}
+}
+alert(`The level of the element is: ${index}`)
